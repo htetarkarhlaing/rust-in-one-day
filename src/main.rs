e7212cf6a32173c097882dbaa1eb0ac2867e64d1
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-11-27 18:44:16 +0630

    Rust for loop

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,28 @@
 
 fn main() {
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let mut loop_index = 0;
+    // let mut loop_index = 0;
 
-    loop {
-        if arr[loop_index] % 2 == 0 {
-            loop_index += 1;
-            continue;
-        }
-        if arr[loop_index] == 9 {
-            break;
-        }
-        println!("Val : {}", arr[loop_index]);
-        loop_index += 1;
-    }
+    // loop {
+    //     if arr[loop_index] % 2 == 0 {
+    //         loop_index += 1;
+    //         continue;
+    //     }
+    //     if arr[loop_index] == 9 {
+    //         break;
+    //     }
+    //     println!("Val : {}", arr[loop_index]);
+    //     loop_index += 1;
+    // }
 
-    // while loop
-    while loop_index < arr.len() {
-        println!("Arr : {}", arr[loop_index]);
-        loop_index += 1;
+    // // while loop
+    // while loop_index < arr.len() {
+    //     println!("Arr : {}", arr[loop_index]);
+    //     loop_index += 1;
+    // }
+
+    // for loop
+    for val in arr.iter() {
+        println!("Val : {}", val)
     }
 }
