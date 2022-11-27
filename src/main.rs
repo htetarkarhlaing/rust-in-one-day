d7dcb9420948d01926ae47adb18179fffd89a385
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-11-27 18:48:46 +0630

    Tuple

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,9 @@
 // use std::cmp::Ordering;
 
 fn main() {
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    // let mut loop_index = 0;
+    let my_tuple: (u8, String, f64) = (47, "John Snow".to_string(), 50_000.005);
+    println!("Name: {}", my_tuple.1);
 
-    // loop {
-    //     if arr[loop_index] % 2 == 0 {
-    //         loop_index += 1;
-    //         continue;
-    //     }
-    //     if arr[loop_index] == 9 {
-    //         break;
-    //     }
-    //     println!("Val : {}", arr[loop_index]);
-    //     loop_index += 1;
-    // }
-
-    // // while loop
-    // while loop_index < arr.len() {
-    //     println!("Arr : {}", arr[loop_index]);
-    //     loop_index += 1;
-    // }
-
-    // for loop
-    for val in arr.iter() {
-        println!("Val : {}", val)
-    }
+    let (v1, v2, v3) = my_tuple;
+    println!("Age: {} and loan: {} is {}", v1, v3, v2);
 }
