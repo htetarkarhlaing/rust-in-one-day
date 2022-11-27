fa5391de273ea57db66f35502809528e8a82c1e8
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-11-27 19:59:14 +0630

    Func part 2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-fn say_hello() {
-    println!("Hello World");
+fn sum_list(list: &[i32]) -> i32 {
+    let mut sum = 0;
+    for &val in list.iter() {
+        sum += &val;
+    }
+    return sum;
 }
 
-fn get_sum(x: u32, y: u32) {
-    println!("{} + {} = {}", x, y, x + y)
-}
-
-fn get_sum_value(x: i32, y: i32) -> i32 {
-    x + y
+fn get_two(x: i32) -> (i32, i32) {
+    return (x + 1, x + 2);
 }
 fn main() {
-    say_hello();
-    get_sum(5, 6);
-    println!("{}", get_sum_value(5, 6));
+    println!("{:?}", get_two(4));
+    let (val1, val2) = get_two(5);
+    println!("Here we go two values, {} & {}", val1, val2);
+
+    let num_list: Vec<i32> = vec![1, 2, 3, 4];
+    println!("Total : {}", sum_list(&num_list))
 }
